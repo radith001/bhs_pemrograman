3f014d7d6e4f47d3444a3a870574c6352e14a360
radith001 <radit.1126@gmail.com>
2024-05-14 09:28:08 +0700

    hay

---
+++ pert4/for.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+//maksudnya gimana?
+
+int main() {
+
+    cout << "iteration statement" << endl;
+    cout << "penggunaan for" << endl;
+
+    //int a = 1;
+    // apa bedanya int diluar for dengan di dalam for?
+
+    for(int a = 1; a <=5; a++){
+        cout << a << endl;
+    }
+
+    return 0;
+}
